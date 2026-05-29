52a00e746cbee0fd650fe738b0be11840ad08197
kazuki-jp-0406 <lihuiyanben5@gmail.com>
2026-05-29 11:56:34 +0900

    LINE送信テスト

--- generate.py
+++ generate.py
@@ -1,1 +1,19 @@
-print("テスト：Pythonが動いています")
+import os
+import requests
+
+token = os.environ["LINE_TOKEN"]
+user_id = os.environ["LINE_USER_ID"]
+
+headers = {
+    "Authorization": "Bearer " + token,
+    "Content-Type": "application/json"
+}
+
+data = {
+    "to": user_id,
+    "messages": [{"type": "text", "text": "テスト：LINEに届きました！"}]
+}
+
+r = requests.post("https://api.line.me/v2/bot/message/push", headers=headers, json=data)
+print(r.status_code)
+print(r.text)
